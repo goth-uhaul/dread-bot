73fe91bcb4a405b657aa85121c67c3743e71e570
Izzychu <izzychu222@gmail.com>
2022-09-06 13:05:45 -0400

    Verify command optimization

--- commands/verify.js
+++ commands/verify.js
@@ -13,12 +13,12 @@ module.exports = {
         .setDescription('Verify yourself on the wiki'),
     execute(interaction) {
         return new Promise(async (resolve, reject) => {
+            await interaction.guild.roles.fetch();
+            if (interaction.member.roles.cache.has(contributorRole)) return interaction.reply('You already have the contributor role!').then(resolve()).catch(e => reject(e));
+
             let user = await fetchUser(interaction.user.id);
             if (!user) return interaction.reply('No user found!').then(resolve()).catch(e => reject(e));
-            
-            await interaction.guild.roles.fetch();
-            
-            if (interaction.member.roles.cache.has(contributorRole)) return interaction.reply('You already have the contributor role!').then(resolve()).catch(e => reject(e));
+
             await interaction.member.roles.add(contributorRole).catch(e => reject(e));
             interaction.reply('User found! You should now have the contributor role.').then(resolve()).catch(e => reject(e));
         });
