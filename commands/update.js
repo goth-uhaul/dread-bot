c0266f1d73764f219b0dd17f3b1608ff671f648b
Izzychu <izzychu222@gmail.com>
2022-09-09 02:04:29 -0400

    Update command

---
+++ commands/update.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { exec } = require('child_process');
+const execAsync = require('util').promisify(exec);
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('update')
+        .setDescription('Update bot/reload commands'),
+    component: 'functionality',
+	ownerOnly: true,
+	execute(interaction) {
+		return new Promise(async (resolve, reject) => {
+			const { err, stdout } = await execAsync('git fetch --all && git reset --hard origin/main');
+            if (err) {
+                return interaction.reply(err.message).then(resolve()).catch((e) => reject(e));
+            }
+            else {
+                await interaction.reply('Rebooting...').then(resolve()).catch((e) => reject(e));
+                process.exit(0);
+            }
+		});
+	}
+};
