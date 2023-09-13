14844ecf4279ca78e6f4035f93fcebf925e3110a
Mayberry <mayberryzoom@gmail.com>
2023-09-12 23:27:40 -0400

    Include reason when adding and removing roles

--- selectMenus/addUserRolesSelection.js
+++ selectMenus/addUserRolesSelection.js
@@ -1,5 +1,14 @@
 const { RoleSelectMenuBuilder } = require('discord.js');
 
+const formatRoles = (roles) => {
+    if (roles.length === 1) return '@' + roles[0];
+    else if (roles.length === 2) return '@' + roles[0] + ' and @' + roles[1];
+    else if (roles.length > 2) {
+        const lastElement = roles.pop();
+        return roles.map(r => '@' + r + ', ').join() + '@' + lastElement;
+    }
+}
+
 module.exports = {
     selectMenu: (id) => new RoleSelectMenuBuilder()
         .setCustomId('addUserRolesSelection_' + id)
@@ -18,7 +27,7 @@ module.exports = {
 
             if (!interaction.roles.find(r => r.id !== interaction.guild.roles.everyone.id && !oldRoles.has(r.id))) return interaction.reply({ content: 'No changes made, as no new roles were selected.', ephemeral: true }).then(resolve()).catch(e => reject(e));
 
-            const newMember = await member.roles.add(interaction.roles).catch(e => reject(e));
+            const newMember = await member.roles.add(interaction.roles, formatRoles(interaction.roles.filter(r => !oldRoles.has(r.id)).map(r => r.name)) + ' added by ' + interaction.user.username + '.').catch(e => reject(e));
             interaction.reply({ content: member.user.toString() + '\'s roles updated successfully.\n\nRoles added: ' + newMember.roles.cache.difference(oldRoles).map(r => r.toString()), ephemeral: true }).then(resolve()).catch(e => reject(e));
         });
     }
--- selectMenus/removeUserRolesSelection.js
+++ selectMenus/removeUserRolesSelection.js
@@ -1,5 +1,14 @@
 const { RoleSelectMenuBuilder } = require('discord.js');
 
+const formatRoles = (roles) => {
+    if (roles.length === 1) return '@' + roles[0];
+    else if (roles.length === 2) return '@' + roles[0] + ' and @' + roles[1];
+    else if (roles.length > 2) {
+        const lastElement = roles.pop();
+        return roles.map(r => '@' + r + ', ').join() + '@' + lastElement;
+    }
+}
+
 module.exports = {
     selectMenu: (id) => new RoleSelectMenuBuilder()
         .setCustomId('removeUserRolesSelection_' + id)
@@ -18,7 +27,7 @@ module.exports = {
 
             if (!oldRoles.find(r => interaction.roles.has(r.id))) return interaction.reply({ content: 'No changes made, as no existing roles were selected.', ephemeral: true }).then(resolve()).catch(e => reject(e));
 
-            const newMember = await member.roles.remove(interaction.roles).catch(e => reject(e));
+            const newMember = await member.roles.remove(interaction.roles, formatRoles(interaction.roles.filter(r => oldRoles.has(r.id)).map(r => r.name)) + ' removed by ' + interaction.user.username + '.').catch(e => reject(e));
             interaction.reply({ content: member.user.toString() + '\'s roles updated successfully.\n\nRoles removed: ' + newMember.roles.cache.difference(oldRoles).map(r => r.toString()), ephemeral: true }).then(resolve()).catch(e => reject(e));
         });
     }
