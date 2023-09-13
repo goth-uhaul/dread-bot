a6bdf2689b0f1c224aca99ceda8652e8ba3a8c75
Mayberry <mayberryzoom@gmail.com>
2023-09-12 22:09:07 -0400

    Properly detect non-applicable selections for roles command select menus

--- selectMenus/addUserRolesSelection.js
+++ selectMenus/addUserRolesSelection.js
@@ -16,8 +16,7 @@ module.exports = {
             const member = await interaction.guild.members.fetch(interaction.customId.slice(22)).catch(e => reject(e));
             const oldRoles = member.roles.cache;
 
-            console.log(oldRoles.map(r => r.name), interaction.roles.map(r => r.name), oldRoles.intersect(interaction.roles).map(r => r.name));
-            if (oldRoles.intersect(interaction.roles).size === interaction.roles.size) return interaction.reply({ content: 'No changes made, as no new roles were selected.', ephemeral: true }).then(resolve()).catch(e => reject(e));
+            if (!interaction.roles.find(r => r.id !== interaction.guild.roles.everyone.id && !oldRoles.has(r.id))) return interaction.reply({ content: 'No changes made, as no new roles were selected.', ephemeral: true }).then(resolve()).catch(e => reject(e));
 
             const newMember = await member.roles.add(interaction.roles).catch(e => reject(e));
             interaction.reply({ content: member.user.toString() + '\'s roles updated successfully.\n\nRoles added: ' + newMember.roles.cache.difference(oldRoles).map(r => r.toString()), ephemeral: true }).then(resolve()).catch(e => reject(e));
--- selectMenus/removeUserRolesSelection.js
+++ selectMenus/removeUserRolesSelection.js
@@ -16,8 +16,7 @@ module.exports = {
             const member = await interaction.guild.members.fetch(interaction.customId.slice(25)).catch(e => reject(e));
             const oldRoles = member.roles.cache;
 
-            console.log(oldRoles.map(r => r.name), interaction.roles.map(r => r.name), oldRoles.difference(interaction.roles).map(r => r.name));
-            if (oldRoles.difference(interaction.roles).size === interaction.roles.size) return interaction.reply({ content: 'No changes made, as no existing roles were selected.', ephemeral: true }).then(resolve()).catch(e => reject(e));
+            if (!oldRoles.find(r => interaction.roles.has(r.id))) return interaction.reply({ content: 'No changes made, as no existing roles were selected.', ephemeral: true }).then(resolve()).catch(e => reject(e));
 
             const newMember = await member.roles.remove(interaction.roles).catch(e => reject(e));
             interaction.reply({ content: member.user.toString() + '\'s roles updated successfully.\n\nRoles removed: ' + newMember.roles.cache.difference(oldRoles).map(r => r.toString()), ephemeral: true }).then(resolve()).catch(e => reject(e));
