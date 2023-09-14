838b55d00ba1eaa479a10acd6fe79446ff2bcc9f
Mayberry <mayberryzoom@gmail.com>
2023-09-13 22:39:15 -0400

    Default channel argument to current channel in channel commands

--- commands/channel.js
+++ commands/channel.js
@@ -10,43 +10,41 @@ module.exports = {
         .addSubcommand(subcommand =>
             subcommand.setName('slowmode')
             .setDescription('Sets the channel\'s slowmode')
-            .addChannelOption(option =>
-                option.setName('channel')
-                .setDescription('The channel to update')
-                .setRequired(true))
             .addNumberOption(option =>
                 option.setName('cooldown')
                 .setDescription('How long users must wait between messages, in seconds')
                 .setMinValue(0)
                 .setMaxValue(216000)
-                .setRequired(true)))
+                .setRequired(true))
+            .addChannelOption(option =>
+                option.setName('channel')
+                .setDescription('The channel to update')))
         .addSubcommand(subcommand =>
             subcommand.setName('rename')
             .setDescription('Changes the channel\'s name')
-            .addChannelOption(option =>
-                option.setName('channel')
-                .setDescription('The channel to update')
-                .setRequired(true))
             .addStringOption(option =>
                 option.setName('name')
                 .setDescription('The new channel name')
                 .setMinLength(1)
                 .setMaxLength(100)
-                .setRequired(true)))
+                .setRequired(true))
+            .addChannelOption(option =>
+                option.setName('channel')
+                .setDescription('The channel to update')))
         .addSubcommand(subcommand =>
             subcommand.setName('topic')
             .setDescription('Sets the channel\'s description')
             .addChannelOption(option =>
                 option.setName('channel')
-                .setDescription('The channel to update')
-                .setRequired(true))),
+                .setDescription('The channel to update'))),
     component: 'moderation',
     execute(interaction) {
         return new Promise(async (resolve, reject) => {
             if (!interaction.member.roles.cache.has(moderatorRole)) return interaction.reply({ content: 'You don\'t have permission to execute this command!', ephemeral: true }).then(resolve()).catch(reject);
 
             const subcommand = interaction.options.getSubcommand();
-            const channel = interaction.options.getChannel('channel');
+            let channel = interaction.options.getChannel('channel');
+            if (!channel) channel = interaction.channel;
 
             if (subcommand === 'slowmode') {
                 const cd = interaction.options.getNumber('cooldown');
