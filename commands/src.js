4485e6957fa4a413b2b8c126db8264864103c8b6
Mayberry <mayberryzoom@gmail.com>
2023-10-22 20:14:13 -0400

    Refactor /page and /verify commands into /src and /wiki

--- commands/src.js
+++ commands/src.js
@@ -1,11 +1,5 @@
-const axios = require('axios');
 const { SlashCommandBuilder, Collection, ActionRowBuilder } = require('discord.js');
-const { graphQlDomain, contributorRole, srcRole } = require ('../config.json');
-const { wikiToken } = require('../tokens.json');
-
-const userQuery = (id) => '{users{singleByProviderId(providerId:"' + id + '"){id,providerId,name,providerName}}}'
-
-const fetchUser = (id) => new Promise((resolve) => axios.get('http://' + graphQlDomain + '/graphql?query=' + userQuery(id), { headers: { 'Authorization': 'Bearer ' + wikiToken } }).then(res => resolve(res.data.data.users.singleByProviderId)));
+const { srcRole } = require ('../config.json');
 
 const apiKeyWarning = `When you press the button below, you will be prompted by a window to provide your API key from SRC.
 Please ensure you are logged into your SRC account, then follow [this link](<https://www.speedrun.com/settings/api>) and show/copy your key.
@@ -17,30 +11,12 @@ If you still don't feel comfortable sharing your API key, feel free to dismiss t
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('verify')
+        .setName('src')
         .setDescription('Verify your accounts on external services'),
     subcommands: new Collection([
-        ['wiki', {
+        ['verify', {
             data: (sc) =>
-                sc.setName('wiki')
-                .setDescription('Verify your wiki account'),
-            component: 'wiki',
-            execute(interaction) {
-                return new Promise(async (resolve, reject) => {
-                    await interaction.guild.roles.fetch();
-                    if (interaction.member.roles.cache.has(contributorRole)) return interaction.reply({ content: 'You already have the contributor role!', ephemeral: true }).then(resolve()).catch(e => reject(e));
-        
-                    let user = await fetchUser(interaction.user.id);
-                    if (!user) return interaction.reply({ content: 'No user found!', ephemeral: true }).then(resolve()).catch(e => reject(e));
-        
-                    await interaction.member.roles.add(contributorRole).catch(e => reject(e));
-                    interaction.reply('User found! You should now have the contributor role.').then(resolve()).catch(e => reject(e));
-                });
-            }
-        }],
-        ['src', {
-            data: (sc) =>
-                sc.setName('src')
+                sc.setName('verify')
                 .setDescription('Verify your speedrun.com account'),
             component: 'src',
             execute(interaction) {
