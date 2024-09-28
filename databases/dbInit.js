b8684d619402f84852229708daf950917ee2b327
jtyd <rpgmidwife@modestarcade.com>
2024-09-28 01:35:00 -0600

    move database.sqlite to its own dedicated folder

--- databases/dbInit.js
+++ databases/dbInit.js
@@ -1,29 +1,35 @@
-const Sequelize = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { enabledComponents } = require('../config.json');
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { enabledComponents } = require("../config.json");
 
 // Initialize sequelize
-const sequelize = new Sequelize('database', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    storage: 'database.sqlite'
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "./db/database.sqlite",
 });
 
 // Import models
-const models = fs.readdirSync(path.resolve(__dirname, './models')).map(file => {
-    const model = require('./models/' + file);
-    if (enabledComponents.includes(model.component)) return model.model(sequelize, Sequelize.DataTypes);
-})
-	.filter(file => !!file);
+const models = fs
+  .readdirSync(path.resolve(__dirname, "./models"))
+  .map((file) => {
+    const model = require("./models/" + file);
+    if (enabledComponents.includes(model.component))
+      return model.model(sequelize, Sequelize.DataTypes);
+  })
+  .filter((file) => !!file);
 
 // Force sync command line option
-const force = process.argv.includes('--force') || process.argv.includes('-f');
+const force = process.argv.includes("--force") || process.argv.includes("-f");
 
-sequelize.sync({ force }).then(() => {
-    console.log('Database synced!');
+sequelize
+  .sync({ force })
+  .then(() => {
+    console.log("Database synced!");
     console.log(models);
 
     sequelize.close();
-}).catch(console.error);
+  })
+  .catch(console.error);
--- databases/dbObjects.js
+++ databases/dbObjects.js
@@ -1,18 +1,19 @@
-const Sequelize = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { enabledComponents } = require('../config.json');
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { enabledComponents } = require("../config.json");
 
 // Initialize sequelize
-const sequelize = new Sequelize('database', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    storage: 'database.sqlite'
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "./db/database.sqlite",
 });
 
 // Import models
-fs.readdirSync(path.resolve(__dirname, './models')).forEach(file => {
-    const model = require('./models/' + file);
-    if (enabledComponents.includes(model.component)) exports[file.slice(0, -3)] = model.model(sequelize, Sequelize.DataTypes);
+fs.readdirSync(path.resolve(__dirname, "./models")).forEach((file) => {
+  const model = require("./models/" + file);
+  if (enabledComponents.includes(model.component))
+    exports[file.slice(0, -3)] = model.model(sequelize, Sequelize.DataTypes);
 });
